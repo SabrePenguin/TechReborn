6f8a62aad6793377765b99faee9b0236bbec3f4c
SabrePotato <lknofczynski@gmail.com>
2026-01-07 13:09:39 -0700

    Add dust items

--- src/main/java/com/sabrepenguin/techreborn/items/TRItems.java
+++ src/main/java/com/sabrepenguin/techreborn/items/TRItems.java
@@ -1,17 +1,21 @@
 package com.sabrepenguin.techreborn.items;
 
 import com.sabrepenguin.techreborn.Tags;
+import com.sabrepenguin.techreborn.items.materials.ItemDust;
 import com.sabrepenguin.techreborn.items.materials.ItemIngot;
 import net.minecraft.item.Item;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
 @GameRegistry.ObjectHolder(Tags.MODID)
 public class TRItems {
+
     public static final Item ingot = null;
+    public static final Item dust = null;
 
     public static Item[] getItems() {
         final Item[] items = {
-                new ItemIngot()
+                new ItemIngot(),
+                new ItemDust(),
         };
 
         return items;
