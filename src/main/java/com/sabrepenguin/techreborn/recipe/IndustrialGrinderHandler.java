88a3a65f9a3dc04a0083a72e92fdd2be1535f11f
SabrePenguin <lknofczynski@gmail.com>
2026-01-28 09:15:14 -0500

    Add empty IndustrialGrinder

---
+++ src/main/java/com/sabrepenguin/techreborn/recipe/IndustrialGrinderHandler.java
@@ -0,0 +1,14 @@
+package com.sabrepenguin.techreborn.recipe;
+
+import com.google.gson.JsonObject;
+import net.minecraft.util.JsonUtils;
+import net.minecraftforge.common.crafting.JsonContext;
+
+
+public class IndustrialGrinderHandler implements ITRRecipeFactory {
+	@Override
+	public void registerRecipe(JsonObject json, JsonContext context) {
+		JsonObject outputs = JsonUtils.getJsonObject(json, "result");
+//		List<ItemStack> out = Arrays.asList(CraftingHelper.get(outputs), context)
+	}
+}
