0afbc3e44f1bf5bc0a8756b5addb9e9bcb01a5c8
SabrePenguin <lknofczynski@gmail.com>
2026-02-01 21:30:54 -0500

    Update sapling to BlockBush

--- src/main/java/com/sabrepenguin/techreborn/blocks/BlockRubberSapling.java
+++ src/main/java/com/sabrepenguin/techreborn/blocks/BlockRubberSapling.java
@@ -3,32 +3,68 @@ package com.sabrepenguin.techreborn.blocks;
 import com.sabrepenguin.techreborn.Tags;
 import com.sabrepenguin.techreborn.TechReborn;
 import com.sabrepenguin.techreborn.worldgen.WorldGenRubberTrees;
-import net.minecraft.block.BlockSapling;
+import mcp.MethodsReturnNonnullByDefault;
+import net.minecraft.block.BlockBush;
+import net.minecraft.block.IGrowable;
 import net.minecraft.block.SoundType;
+import net.minecraft.block.properties.PropertyInteger;
+import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.NonNullList;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraft.world.gen.feature.WorldGenerator;
 import net.minecraftforge.event.terraingen.TerrainGen;
-import org.jetbrains.annotations.NotNull;
 
+import javax.annotation.ParametersAreNonnullByDefault;
 import java.util.Random;
 
-public class BlockRubberSapling extends BlockSapling {
+@ParametersAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+public class BlockRubberSapling extends BlockBush implements IGrowable {
+	public static final PropertyInteger STAGE = PropertyInteger.create("stage", 0, 1);
+	protected static final AxisAlignedBB SAPLING_AABB = new AxisAlignedBB(0.09999999403953552D, 0.0D, 0.09999999403953552D, 0.8999999761581421D, 0.800000011920929D, 0.8999999761581421D);
+
 	public BlockRubberSapling() {
 		this.setRegistryName(Tags.MODID, "rubber_sapling");
 		this.setTranslationKey(Tags.MODID + ".rubber_sapling");
 		this.setCreativeTab(TechReborn.RESOURCE_TAB);
+		this.setHardness(0.0f);
 		this.setDefaultState(this.getDefaultState().withProperty(STAGE, 0));
 		this.setSoundType(SoundType.PLANT);
 	}
 
 	@Override
-	public void generateTree(@NotNull World worldIn, @NotNull BlockPos pos, @NotNull IBlockState state, @NotNull Random rand) {
+	@SuppressWarnings("deprecation")
+	public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
+		return SAPLING_AABB;
+	}
+
+	@Override
+	public boolean canGrow(World worldIn, BlockPos pos, IBlockState state, boolean isClient) {
+		return true;
+	}
+
+	@Override
+	public boolean canUseBonemeal(World worldIn, Random rand, BlockPos pos, IBlockState state) {
+		return worldIn.rand.nextFloat() < 0.45D;
+	}
+
+	@Override
+	public void grow(World worldIn, Random rand, BlockPos pos, IBlockState state) {
+		if (state.getValue(STAGE) == 0) {
+			worldIn.setBlockState(pos, state.cycleProperty(STAGE), 4);
+		} else {
+			generateTree(worldIn, pos, state, rand);
+		}
+	}
+
+	public void generateTree(World worldIn, BlockPos pos, IBlockState state, Random rand) {
 		if (!TerrainGen.saplingGrowTree(worldIn, rand, pos)) return;
 		WorldGenerator generator = new WorldGenRubberTrees(true);
 		worldIn.setBlockState(pos, Blocks.AIR.getDefaultState(), 4);
@@ -38,5 +74,23 @@ public class BlockRubberSapling extends BlockSapling {
 	@Override
 	public void getSubBlocks(CreativeTabs itemIn, NonNullList<ItemStack> items) {
 		items.add(new ItemStack(this, 1, 0));
+	}
+
+	@Override
+	protected BlockStateContainer createBlockState() {
+		return new BlockStateContainer(this, STAGE);
+	}
+
+	@Override
+	@SuppressWarnings("deprecation")
+	public IBlockState getStateFromMeta(int meta) {
+		return this.getDefaultState().withProperty(STAGE, (meta & 8) >> 3);
+	}
+
+	@Override
+	public int getMetaFromState(IBlockState state) {
+		int meta = 0;
+		meta |= state.getValue(STAGE) << 3;
+		return meta;
 	}
 }
