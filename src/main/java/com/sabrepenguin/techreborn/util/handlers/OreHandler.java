b9abbd8d2917a3be26a41ef374c58b3ccc86e057
SabrePenguin <lknofczynski@gmail.com>
2026-02-03 13:54:43 -0500

    Add stair, plank, and slab oredict

--- src/main/java/com/sabrepenguin/techreborn/util/handlers/OreHandler.java
+++ src/main/java/com/sabrepenguin/techreborn/util/handlers/OreHandler.java
@@ -9,6 +9,10 @@ public class OreHandler {
 		OreDictionary.registerOre("saplingRubber", TRBlocks.rubber_sapling);
 		OreDictionary.registerOre("logWood", TRBlocks.rubber_log);
 		OreDictionary.registerOre("logRubber", TRBlocks.rubber_log);
+		OreDictionary.registerOre("plankWood", TRBlocks.rubber_planks);
+		OreDictionary.registerOre("plankRubber", TRBlocks.rubber_planks);
+		OreDictionary.registerOre("slabWood", TRBlocks.rubber_plank_slab);
+		OreDictionary.registerOre("stairWood", TRBlocks.rubber_plank_stair);
 		OreDictionary.registerOre("treeLeaves", TRBlocks.rubber_leaves);
 		OreDictionary.registerOre("leavesRubber", TRBlocks.rubber_leaves);
 	}
