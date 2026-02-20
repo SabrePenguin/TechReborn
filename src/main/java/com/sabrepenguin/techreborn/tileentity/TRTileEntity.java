6bb873273e47c8da44aa7397a30909eaf6a2ac04
SabrePenguin <lknofczynski@gmail.com>
2026-02-19 22:58:02 -0500

    Register TEElectricFurnace

--- src/main/java/com/sabrepenguin/techreborn/tileentity/TRTileEntity.java
+++ src/main/java/com/sabrepenguin/techreborn/tileentity/TRTileEntity.java
@@ -1,6 +1,7 @@
 package com.sabrepenguin.techreborn.tileentity;
 
 import com.sabrepenguin.techreborn.Tags;
+import com.sabrepenguin.techreborn.tileentity.processing.TileEntityElectricFurnace;
 import com.sabrepenguin.techreborn.tileentity.tier0.TileEntityIronAlloyFurnace;
 import com.sabrepenguin.techreborn.tileentity.tier0.TileEntityIronFurnace;
 import net.minecraft.util.ResourceLocation;
@@ -10,5 +11,6 @@ public class TRTileEntity {
 	public static void registerTileEntity() {
 		GameRegistry.registerTileEntity(TileEntityIronFurnace.class, new ResourceLocation(Tags.MODID, "iron_furnace"));
 		GameRegistry.registerTileEntity(TileEntityIronAlloyFurnace.class, new ResourceLocation(Tags.MODID, "iron_alloy_furnace"));
+		GameRegistry.registerTileEntity(TileEntityElectricFurnace.class, new ResourceLocation(Tags.MODID, "electric_furnace"));
 	}
 }
