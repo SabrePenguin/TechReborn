374e2de63616081a525cd6052374e70f2913cfe3
SabrePenguin <lknofczynski@gmail.com>
2026-02-23 21:58:38 -0500

    Set configurinator to be block-specific w/ display name

--- src/main/java/com/sabrepenguin/techreborn/items/tools/ItemConfigurinator.java
+++ src/main/java/com/sabrepenguin/techreborn/items/tools/ItemConfigurinator.java
@@ -5,14 +5,18 @@ import com.sabrepenguin.techreborn.TechReborn;
 import com.sabrepenguin.techreborn.capability.stackhandler.ISideConfigTE;
 import com.sabrepenguin.techreborn.util.INonStandardLocation;
 import mcp.MethodsReturnNonnullByDefault;
+import net.minecraft.block.Block;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.SoundEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.*;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
 
 import javax.annotation.ParametersAreNonnullByDefault;
@@ -56,30 +60,57 @@ public class ItemConfigurinator extends Item implements INonStandardLocation {
 			ItemStack stack = player.getHeldItem(hand);
 			if (stack.getItem() != this)
 				return EnumActionResult.PASS;
-			if (!world.isRemote) {
-				if (player.isSneaking()) {
+			IBlockState state = world.getBlockState(pos);
+			Block block = state.getBlock();
+			String regName = block.getRegistryName().toString();
+			ItemBlock.getItemFromBlock(block).getTranslationKey(stack);
+			if (player.isSneaking()) {
+				if (!world.isRemote) {
 					NBTTagCompound compound = new NBTTagCompound();
 					compound.setTag("IOManager", sideConfigTE.getManager().writeToNBT());
+					compound.setString("Block", regName);
 					stack.setTagCompound(compound);
 				} else {
-					if (stack.hasTagCompound()) {
-						NBTTagCompound compound = Objects.requireNonNull(stack.getTagCompound()).getCompoundTag("IOManager");
-						if (!compound.isEmpty()) {
-							sideConfigTE.getManager().readFromNBT(compound);
-							sideConfigTE.updateTE();
+					world.playSound(player, pos, SoundEvents.ENTITY_EXPERIENCE_ORB_PICKUP, SoundCategory.BLOCKS, 1.0f, 1.0f);
+				}
+				player.swingArm(hand);
+				return EnumActionResult.SUCCESS;
+			} else {
+				if (stack.hasTagCompound()) {
+					NBTTagCompound stackCompound = stack.getTagCompound();
+					if (stackCompound != null && stackCompound.getString("Block").equals(regName)) {
+						if (!world.isRemote) {
+							NBTTagCompound compound = stackCompound.getCompoundTag("IOManager");
+							if (!compound.isEmpty()) {
+								sideConfigTE.getManager().readFromNBT(compound);
+								sideConfigTE.updateTE();
+							}
+						} else {
+							world.playSound(player, pos, SoundEvents.UI_BUTTON_CLICK, SoundCategory.BLOCKS, 1.0f, 1.0f);
 						}
+						player.swingArm(hand);
+						return EnumActionResult.SUCCESS;
 					}
 				}
-			} else {
-				if (player.isSneaking()) {
-					world.playSound(player, pos, SoundEvents.ENTITY_EXPERIENCE_ORB_PICKUP, SoundCategory.BLOCKS, 1.0f, 1.0f);
-				} else {
-					world.playSound(player, pos, SoundEvents.UI_BUTTON_CLICK, SoundCategory.BLOCKS, 1.0f, 1.0f);
-				}
 			}
-			player.swingArm(hand);
-			return EnumActionResult.SUCCESS;
 		}
 		return EnumActionResult.PASS;
+	}
+
+	@Override
+	public String getItemStackDisplayName(ItemStack stack) {
+		String baseName = super.getItemStackDisplayName(stack);
+		if (stack.hasTagCompound() && stack.getTagCompound().hasKey("Block")) {
+			NBTTagCompound stackCompound = stack.getTagCompound();
+			String regName = stackCompound.getString("Block");
+
+			Block block = Block.getBlockFromName(regName);
+			if (block != null) {
+				ItemStack blockStack = new ItemStack(block, 1);
+				String blockName = blockStack.getDisplayName();
+				return TextFormatting.AQUA + baseName + " (" + blockName + ")";
+			}
+		}
+		return baseName;
 	}
 }
