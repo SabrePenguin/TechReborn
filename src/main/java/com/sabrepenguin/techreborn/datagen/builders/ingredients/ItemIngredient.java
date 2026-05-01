8cc8acd52d193d3cc0b2bdf243dbad30be1cfc54
SabrePenguin <lknofczynski@gmail.com>
2026-05-01 16:37:33 -0400

    Allow nbt on items

--- src/main/java/com/sabrepenguin/techreborn/datagen/builders/ingredients/ItemIngredient.java
+++ src/main/java/com/sabrepenguin/techreborn/datagen/builders/ingredients/ItemIngredient.java
@@ -1,10 +1,8 @@
 package com.sabrepenguin.techreborn.datagen.builders.ingredients;
 
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonSerializationContext;
-import com.google.gson.JsonSerializer;
+import com.google.gson.*;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.*;
 
 import java.lang.reflect.Type;
 
@@ -25,7 +23,36 @@ public class ItemIngredient implements IBasicIngredient {
 			}
 			if (src.stack.getCount() > 1)
 				element.addProperty("count", src.stack.getCount());
+			if (src.stack.hasTagCompound()) {
+				element.add("nbt", nbtToJson(src.stack.getTagCompound()));
+			}
 			return element;
+		}
+
+		private JsonElement nbtToJson(NBTBase nbt) {
+			if (nbt instanceof NBTTagCompound tag) {
+				JsonObject object = new JsonObject();
+				for (String key: tag.getKeySet()) {
+					object.add(key, nbtToJson(tag.getTag(key)));
+				}
+				return object;
+			}
+			if (nbt instanceof NBTTagList list) {
+				JsonArray array = new JsonArray();
+				for (int i = 0; i < list.tagCount(); i++) {
+					array.add(nbtToJson(list.get(i)));
+				}
+				return array;
+			}
+			if (nbt instanceof NBTTagString string)
+				return new JsonPrimitive(string.getString());
+			if (nbt instanceof NBTTagDouble d)
+				return new JsonPrimitive(d.getDouble());
+			if (nbt instanceof NBTTagFloat f)
+				return new JsonPrimitive(f.getFloat());
+			if (nbt instanceof NBTPrimitive primitive)
+				return new JsonPrimitive(primitive.getLong());
+			return JsonNull.INSTANCE;
 		}
 	}
 }
